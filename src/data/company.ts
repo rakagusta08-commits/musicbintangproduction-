b0f02184ea8c908b642a5bb4a19f8b11a598223d
rakagusta08-commits <rakagusta08@gmail.com>
2026-09-01 20:09:10 +0700

    Update company contact phone number to +62 831-9175-3436

--- src/data/company.ts
+++ src/data/company.ts
@@ -10,7 +10,7 @@ export const company: Company = {
     "membangun jaringan kolaboratif antara musisi, produser, dan pelaku industri kreatif."
   ],
   contact: {
-    phone: "+62 821-1550-0341",
+    phone: "+62 831-9175-3436",
     email: "musicbintangproduction@gmail.com",
     address: "Jl. Riung Purna 9. No. 13\nKomplek Riung Bandung"
   }
